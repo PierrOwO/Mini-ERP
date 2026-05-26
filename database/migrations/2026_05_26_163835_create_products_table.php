dc42f613e4c24dca56451eba11cc8c8d2db87ecc
PierrOwO <piotrekmilos@gmail.com>
2026-05-26 18:54:10 +0200

    installed sanctum

--- database/migrations/2026_05_26_163835_create_products_table.php
+++ database/migrations/2026_05_26_163835_create_products_table.php
@@ -13,7 +13,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('sku')->unique();
+
+            $table->text('description')->nullable();
+
+            $table->decimal('price', 10, 2);
+
+            $table->integer('quantity')->default(0);
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
